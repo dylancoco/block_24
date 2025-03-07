5e666f7e71157a78f9b4bd9e3963b2703918188c
Dylan Cocoletzi <dcocoletzi49@gmail.com>
2025-03-06 22:59:25 -0800

    Tab 3

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -4,15 +4,31 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log("Puppies", puppies)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId) 
+  console.log("Featured Pup", featuredPup)
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return (
+              <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+                {puppy.name}
+              </p>
+            )
           })
         }
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
